5839c7d2ce210065e4b853d90f6547486a3c91b1
Julio Zambom <work.juliozambom@gmail.com>
2022-06-20 14:35:03 -0300

    config: setting up styled components and theme global styles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,52 @@
-import { Home } from './src/screens/Home';
+import { useState, useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import {
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+  Montserrat_800ExtraBold,
+  Montserrat_900Black,
+} from '@expo-google-fonts/montserrat';
 
-const App = () => <Home />;
+import { ThemeProvider } from 'styled-components/native';
+
+import { Home } from './src/screens/Home';
+import theme from './src/global/styles/theme';
+
+const App = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({
+          Montserrat_500Medium,
+          Montserrat_600SemiBold,
+          Montserrat_700Bold,
+          Montserrat_800ExtraBold,
+          Montserrat_900Black,
+        });
+      } catch (e) {
+        // eslint-disable-next-line no-console
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        SplashScreen.hideAsync();
+      }
+    })();
+  }, []);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+};
 
 export default App;
