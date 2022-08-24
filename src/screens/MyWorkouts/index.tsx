aed01d13134f7e02b29f0528ac4acad623f8237e
Diego Arruda <arrudadiego45@gmail.com>
2022-08-24 10:51:32 -0300

    feat: creating screen switcher of the myworkouts screen

--- src/screens/MyWorkouts/index.tsx
+++ src/screens/MyWorkouts/index.tsx
@@ -1,14 +1,44 @@
+import { useState } from 'react';
+import { StatusBar } from 'react-native';
 import { Heading } from '../../components/Heading';
 import { NavigationProps } from '../../types/NavigationProps';
-import { Container } from './styles';
+import {
+  Container,
+  Switcher,
+  SwitcherButton,
+  SwitcherIndicator,
+  SwitcherText,
+} from './styles';
 
-export const MyWorkouts = ({ navigation }: NavigationProps) => (
-  <>
-    <Heading
-      title="Meus treinos"
-      onGoBack={() => navigation.goBack()}
-      onPressConfig={() => navigation.navigate('Configurations')}
-    />
-    <Container />
-  </>
-);
+export const MyWorkouts = ({ navigation }: NavigationProps) => {
+  // if active, free workouts is showing, else, workouts plan is showing
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <>
+      <StatusBar backgroundColor="#EBF2FF" />
+      <Heading
+        title="Meus treinos"
+        onGoBack={() => navigation.goBack()}
+        onPressConfig={() => navigation.navigate('Configurations')}
+      />
+      <Container>
+        <Switcher>
+          <SwitcherButton onPress={() => setIsActive(false)}>
+            <SwitcherText isBlack={!isActive}>Plano de treino</SwitcherText>
+          </SwitcherButton>
+          <SwitcherButton onPress={() => setIsActive(true)}>
+            <SwitcherText isBlack={isActive}>Treinos livres</SwitcherText>
+          </SwitcherButton>
+          <SwitcherIndicator
+            from={{ left: isActive ? '2%' : '53%' }}
+            animate={{ left: isActive ? '53%' : '2%' }}
+            transition={{
+              type: 'timing',
+              duration: 200,
+            }}
+          />
+        </Switcher>
+      </Container>
+    </>
+  );
+};
--- src/screens/MyWorkouts/styles.ts
+++ src/screens/MyWorkouts/styles.ts
@@ -1,7 +1,49 @@
+import { RFValue } from 'react-native-responsive-fontsize';
+import { MotiView } from 'moti';
 import styled from 'styled-components/native';
+
+interface SwitcherProps {
+  isBlack?: boolean;
+}
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.blue[100]};
   padding: 0 24px;
+  align-items: center;
+`;
+
+export const Switcher = styled.View`
+  background-color: ${({ theme }) => theme.colors.blue[500]};
+  height: ${RFValue(36)}px;
+  width: 70%;
+  border-radius: 999px;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: row;
+  padding: 0 6px;
+`;
+
+export const SwitcherButton = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})`
+  width: 60%;
+  align-items: center;
+`;
+
+export const SwitcherIndicator = styled(MotiView)`
+  background-color: ${({ theme }) => theme.colors.blue[100]};
+  height: ${RFValue(28)}px;
+  width: 50%;
+  border-radius: 999px;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  z-index: -1;
+`;
+
+export const SwitcherText = styled.Text<SwitcherProps>`
+  font-family: ${({ theme }) => theme.fonts.bold};
+  font-size: ${RFValue(10)}px;
+  color: ${({ isBlack }) => (isBlack ? '#000' : '#FFF')};
 `;
