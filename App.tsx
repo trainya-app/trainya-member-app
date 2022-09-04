bb7d3c008ba34c40cc254805351e138413cfaaaa
Diego Arruda <arrudadiego45@gmail.com>
2022-09-03 22:37:16 -0300

    feat: creating authcontext

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { StatusBar } from 'react-native';
 import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import {
@@ -12,12 +13,11 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 
 import { ThemeProvider } from 'styled-components/native';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { ThemeContextProvider } from './src/contexts/ThemeContext';
 import { AppRoutes } from './src/routes/app.routes';
 
-import { ThemeContextProvider } from './src/contexts/ThemeContext';
-
 import theme from './src/global/styles/theme';
-import { StatusBar } from 'react-native';
 
 const App = () => {
   const [colorMode, setColorMode] = useState('light' as 'light' | 'dark');
@@ -49,14 +49,16 @@ const App = () => {
     return null;
   }
   return (
-    <ThemeContextProvider colorMode={colorMode} setColorMode={setColorMode}>
-      <ThemeProvider theme={theme[colorMode]}>
-        <StatusBar backgroundColor="#000" translucent/>
-        <NavigationContainer>
-          <AppRoutes />
-        </NavigationContainer>
-      </ThemeProvider>
-    </ThemeContextProvider>
+    <AuthContextProvider>
+      <ThemeContextProvider colorMode={colorMode} setColorMode={setColorMode}>
+        <ThemeProvider theme={theme[colorMode]}>
+          <StatusBar backgroundColor="#000" translucent />
+          <NavigationContainer>
+            <AppRoutes />
+          </NavigationContainer>
+        </ThemeProvider>
+      </ThemeContextProvider>
+    </AuthContextProvider>
   );
 };
 
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useMemo, useState } from 'react';
+
+interface Props {
+  children: ReactNode;
+}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const AuthContext = createContext({} as any);
+
+export const AuthContextProvider = ({ children }: Props) => {
+  const [user, setUser] = useState('');
+
+  async function login({
+    email,
+    password,
+  }: {
+    email: string;
+    password: string;
+  }) {
+    try {
+      if (email === 'trainya@app.com' && password === 'trainya') {
+        setUser(email);
+      } else {
+        console.log('Algo deu errado');
+      }
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log('Algo deu errado');
+    }
+  }
+
+  const AuthContextProviderValue = useMemo(
+    () => ({ user, login }),
+    [user, login]
+  );
+
+  return (
+    <AuthContext.Provider value={AuthContextProviderValue}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
