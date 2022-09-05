b0d736c5d63c0df355769a9faadcd88ea4685176
Diego Arruda <arrudadiego45@gmail.com>
2022-09-05 18:33:02 -0300

    fix: adding the user informations in the authcontext

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useEffect, useMemo, useState } from 'react';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import jwt_decode from 'jwt-decode';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ToastAndroid } from 'react-native';
 
@@ -7,17 +8,41 @@ import { api } from '../services/api';
 interface Props {
   children: ReactNode;
 }
+
+interface UserProps {
+  name: string;
+  email: string;
+  height: number;
+  weight: number;
+  phone: string;
+  state: string;
+  street: string;
+  city: string;
+}
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const AuthContext = createContext({} as any);
 
 export const AuthContextProvider = ({ children }: Props) => {
   const [token, setToken] = useState<string | null>();
+  const [userDecoded, setUserDecoded] = useState<UserProps>({
+    name: '',
+    email: '',
+    height: 0,
+    weight: 0,
+    phone: '',
+    state: '',
+    street: '',
+    city: '',
+  });
 
   useEffect(() => {
     (async () => {
       const storagedToken = await AsyncStorage.getItem('@trainyaapp:token');
       if (storagedToken) {
-        setToken(storagedToken);
+        setToken(JSON.parse(storagedToken));
+        api.defaults.headers.Authorization = `Bearer ${JSON.parse(
+          storagedToken
+        )}}`;
       }
     })();
   }, []);
@@ -34,8 +59,17 @@ export const AuthContextProvider = ({ children }: Props) => {
   }) {
     try {
       const { data } = await api.post('auth/members', { email, password });
+
       setToken(data.token);
-      await AsyncStorage.setItem('@trainyaapp:token', data.token);
+      await AsyncStorage.setItem(
+        '@trainyaapp:token',
+        JSON.stringify(data.token)
+      );
+
+      api.defaults.headers.Authorization = `Bearer ${data.token}`;
+
+      setUserDecoded(jwt_decode(data.token));
+
       showToast('Login realizado com sucesso!');
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
@@ -49,13 +83,18 @@ export const AuthContextProvider = ({ children }: Props) => {
     });
   }
 
-  const AuthContextProviderValue = useMemo(
-    () => ({ token, login, logout }),
-    [token, login, logout]
-  );
-
   return (
-    <AuthContext.Provider value={AuthContextProviderValue}>
+    <AuthContext.Provider
+      // eslint-disable-next-line react/jsx-no-constructed-context-values
+      value={{
+        token,
+        user: {
+          name: userDecoded.name,
+        },
+        login,
+        logout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
