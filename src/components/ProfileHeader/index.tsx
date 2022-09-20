9f2548ef83717eb633059173a882d1dba8a38e05
Julio Zambom <work.juliozambom@gmail.com>
2022-09-19 22:02:39 -0300

    feat(ProfileHeaderComponent): consuming api data

--- src/components/ProfileHeader/index.tsx
+++ src/components/ProfileHeader/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react';
 import {
   Header,
   ProfileImageContainer,
@@ -9,29 +10,32 @@ import {
 } from './styles';
 
 import { Button } from '../Button';
+import { AuthContext } from '../../contexts/AuthContext';
 
 interface ProfileHeaderProps {
   onPressChangePhoto: () => void;
 }
-export const ProfileHeader= ({ onPressChangePhoto }: ProfileHeaderProps) => (
+export const ProfileHeader = ({ onPressChangePhoto }: ProfileHeaderProps) => {
+  const { user } = useContext(AuthContext);
+  return (
     <Header>
-          <ProfileImageContainer>
-            <ProfilePhoto source={{ uri: 'https://i.imgur.com/nRsfy96.png' }} />
-            <ChangePhotoIconContainer>
-              <ChangePhotoIcon />
-            </ChangePhotoIconContainer>
-          </ProfileImageContainer>
-          <ProfileInfoContainer>
-            <Username>Mariana Santos</Username>
-            <Button
-              title="Editar informações"
-              width={70}
-              height={36}
-              style={{ marginTop: 12 }}
-              fontSize={11}
-              onPress={onPressChangePhoto}
-            />
-          </ProfileInfoContainer>
-        </Header>
-  )
-
+      <ProfileImageContainer>
+        <ProfilePhoto source={{ uri: 'https://i.imgur.com/nRsfy96.png' }} />
+        <ChangePhotoIconContainer>
+          <ChangePhotoIcon />
+        </ChangePhotoIconContainer>
+      </ProfileImageContainer>
+      <ProfileInfoContainer>
+        <Username>{user.name}</Username>
+        <Button
+          title="Editar informações"
+          width={70}
+          height={36}
+          style={{ marginTop: 12 }}
+          fontSize={11}
+          onPress={onPressChangePhoto}
+        />
+      </ProfileInfoContainer>
+    </Header>
+  );
+};
