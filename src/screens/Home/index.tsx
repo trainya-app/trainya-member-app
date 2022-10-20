dcec1f5c02a4e24554e2cd42820312f0baf533ac
Julio Zambom <work.juliozambom@gmail.com>
2022-10-19 21:55:10 -0300

    feat: adding navigation to see workouts home button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -179,6 +179,7 @@ export const Home = ({ navigation }: Props) => {
               height={40}
               style={{ marginTop: 24, marginBottom: 24 }}
               fontSize={13}
+              onPress={() => navigation.navigate('MyWorkoutsStack')}
             />
 
             <CardContainer>
